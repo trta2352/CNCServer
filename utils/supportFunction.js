57b6296d67230d441fa204f8bd8f90eea9e1a753
Jaka Tertinek <tertinek.jaka@gmail.com>
2019-10-14 17:20:20 +0200

    Update supportFunction.js

--- utils/supportFunction.js
+++ utils/supportFunction.js
@@ -2,9 +2,10 @@
 module.exports = SupportFun = {
     modifiyArray: function (jsonArray){
         let modifiedArray = [];
+        console.log(jsonArray)
         if(Array.isArray(jsonArray)){
             for(var i = 0; i < jsonArray.length; i++){
-                modifiedArray.push(jsonArray.recipients[i].email)
+                modifiedArray.push(jsonArray[i].email)
             }
         }
         return modifiedArray;
