f4be3e57c27ff299080f5b38b9681205b50f0724
Jaka Tertinek <tertinek.jaka@gmail.com>
2019-10-14 17:14:32 +0200

    Update jokesController.js

--- controllers/jokesController.js
+++ controllers/jokesController.js
@@ -24,7 +24,7 @@ router.get('/', async (req, res, next) => {
     }
 });
 
-router.post('/', async (req, res, next) => {
+router.post('/', async (req, res, next) => {console.log(req.body)
     let apiUrl = 'http://api.icndb.com/jokes/random/1'
     let modifiedArray = supportFunctions.modifiyArray(req.body)
     try {
